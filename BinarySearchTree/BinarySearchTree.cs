2ec7d8fa8312259c7a335a147745755e0ac01738
DonaldZheng <dzheng112@gmail.com>
2021-03-02 13:27:49 -0600

    set up my code for the BTS project

---
+++ BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class BinarySearchTree
+    {
+        //member variables
+        public Node RootNode;
+
+        // ctor
+        public BinarySearchTree()
+        {
+            
+        }
+
+        // member methods
+        public void AddNode(Node nodeToAdd) // less or greater than root node
+        {
+            if (nodeToAdd.Data > RootNode.Data)
+            {
+                RootNode.RightNode = nodeToAdd;
+            }
+            else
+            {
+                RootNode.LeftNode = nodeToAdd;
+            }
+
+        }
+
+        public bool SearchNode(int dataToFind) // to see if data exisit
+        {
+            return false;
+        }
+    }
+}
---
+++ BinarySearchTree/Node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Node
+    {
+        //member variables
+        public int Data; // info hold on to 
+        public Node RightNode;
+        public Node LeftNode;
+
+        // ctor
+        public Node(int data)
+        {
+            Data = data;
+        }
+        // member methods
+    }
+}
