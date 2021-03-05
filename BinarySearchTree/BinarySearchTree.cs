6c01d6c94409b8301eefd7ed323f096b570f03ec
DonaldZheng <dzheng112@gmail.com>
2021-03-05 08:58:43 -0600

    finished my addNode and working on my SearchNode

--- BinarySearchTree/BinarySearchTree.cs
+++ BinarySearchTree/BinarySearchTree.cs
@@ -20,19 +20,37 @@ namespace BinarySearchTree
         // member methods
         public void AddNode(Node nodeToAdd) // less or greater than root node
         {
-            if (nodeToAdd.Data > RootNode.Data)
+            if (RootNode == null) // if null is empty
             {
-                RootNode.RightNode = nodeToAdd;
+                RootNode = nodeToAdd; // add the node
+                
             }
             else
             {
-                RootNode.LeftNode = nodeToAdd;
+                Node currentNode = RootNode;
+                while (true)
+                {
+                    if (currentNode.LeftNode == null && currentNode.Data > nodeToAdd.Data)// if current node is bigger than ?
+                    {
+                        currentNode.RightNode = nodeToAdd; // add to the left node // reference nevin's example
+                        break; 
+                    }
+                    else if (currentNode.RightNode == null && currentNode.Data < nodeToAdd.Data)
+                    {
+                        currentNode.LeftNode = nodeToAdd;
+                        break;
+                    }
+                }
             }
 
         }
 
         public bool SearchNode(int dataToFind) // to see if data exisit
         {
+            if (RootNode != null) // if node is not empty
+            {
+                if (dataToFind < )
+            }
             return false;
         }
     }
